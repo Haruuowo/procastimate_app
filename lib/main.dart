4bc0c4690cfd422d88cbc380ee8b5ab3393287ad
JCDC0 <legoman1jade@gmail.com>
2025-09-23 18:20:14 +0800

    Firebase boilerplate
    IOS, android, atchaka windows

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Procastimate',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
